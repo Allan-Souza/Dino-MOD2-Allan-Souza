66437c1f9591897d0e4a9651b6c67f7728b9478c
Allan <allanzissimo@gmail.com>
2023-05-23 20:30:46 -0300

    resolução do bug agachamento

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -74,7 +74,7 @@ class Dinosaur(Sprite):
         self.image = DUCK_IMG[self.type][self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS
+        self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
         self.dino_duck = False
         
