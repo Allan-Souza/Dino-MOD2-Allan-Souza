3df7b454742c1b487714420916925f6db55303d1
Allan <allanzissimo@gmail.com>
2023-05-24 15:30:38 -0300

    troca dos sprites

--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -9,16 +9,16 @@ FPS = 30
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
+ICON = pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioWallpaper.png"))
 
 RUNNING = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioWalk1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioWalk2.png")),
 ]
 
 RUNNING_SHIELD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1Shield.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioStarWalk1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioStarWalk2.png")),
 ]
 
 RUNNING_HAMMER = [
@@ -26,18 +26,18 @@ RUNNING_HAMMER = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
 ]
 
-JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJump.png"))
-JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpShield.png"))
+JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioJump.png"))
+JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioStarJump.png"))
 JUMPING_HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpHammer.png"))
 
 DUCKING = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioDuck.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioDuck.png")),
 ]
 
 DUCKING_SHIELD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Shield.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioStarDuck.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/MarioStarDuck.png")),
 ]
 
 DUCKING_HAMMER = [
@@ -46,23 +46,23 @@ DUCKING_HAMMER = [
 ]
 
 SMALL_CACTUS = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/Dragon.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/gumba.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/gumba.png")),
 ]
 LARGE_CACTUS = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus3.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/GreenTube.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/GreenTube.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/GreenTube.png")),
 ]
 
 BIRD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/Canon.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Mario/others/Canon.png")),
 ]
 
 CLOUD = pygame.image.load(os.path.join(IMG_DIR, 'Other/Cloud.png'))
-SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
+SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Mario/others/Flower.png'))
 HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png'))
 
 BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
